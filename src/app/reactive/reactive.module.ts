3c8cf4cb5e3fbe3f9df8a0cad327df700f7f2f3f
Jonathan San Miguel <guerreroz80@outlook.es>
2023-08-02 12:50:47 -0600

    creacion de los templates html de cada form

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchPageComponent } from './pages/switch-page/switch-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchPageComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
